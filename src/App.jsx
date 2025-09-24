68c57d54f8d44c9f775ce4dfdfd854d9eb882fb3
Komal Rout <komalrout.kr@gmail.com>
2025-09-24 23:27:42 +0530

    made folder changes and created completed the web design

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,91 @@ const App = () => {
     },
   ];
 
+  const temperature_details = [
+    { label: "Feels Like", value: "18°" },
+    { label: "Humidity", value: "60%" },
+    { label: "Wind", value: "10 km/h" },
+    { label: "Precipitation", value: "20%" },
+  ];
+
+  const daily_forcast = [
+    {
+      day: "Wed",
+      icon: "public/assets/images/icon-sunny.webp",
+      max_temp: "22°",
+      min_temp: "15°",
+    },
+    {
+      day: "Thu",
+      icon: "public/assets/images/icon-snow.webp",
+      max_temp: "19°",
+      min_temp: "14°",
+    },
+    {
+      day: "Fri",
+      icon: "public/assets/images/icon-rain.webp",
+      max_temp: "21°",
+      min_temp: "16°",
+    },
+    {
+      day: "Sat",
+      icon: "public/assets/images/icon-partly-cloudy.webp",
+      max_temp: "23°",
+      min_temp: "17°",
+    },
+    {
+      day: "Sun",
+      icon: "public/assets/images/icon-sunny.webp",
+      max_temp: "24°",
+      min_temp: "18°",
+    },
+    {
+      day: "Mon",
+      icon: "public/assets/images/icon-storm.webp",
+      max_temp: "20°",
+      min_temp: "15°",
+    },
+    {
+      day: "Tue",
+      icon: "public/assets/images/icon-storm.webp",
+      max_temp: "21°",
+      min_temp: "16°",
+    },
+  ];
+
+  const week_options = [
+    { label: "Monday", value: "Mon", id: "Mon" },
+    { label: "Monday", value: "Mon", id: "Mon" },
+    { label: "Monday", value: "Mon", id: "Mon" },
+  ];
+
+  const hourly_forcast = [
+    {
+      time_stamp: "3 PM",
+      icon: "public/assets/images/icon-sunny.webp",
+      temp: "22°",
+    },
+    {
+      time_stamp: "3 PM",
+      icon: "public/assets/images/icon-sunny.webp",
+      temp: "22°",
+    },
+    {
+      time_stamp: "3 PM",
+      icon: "public/assets/images/icon-sunny.webp",
+      temp: "22°",
+    },
+    {
+      time_stamp: "3 PM",
+      icon: "public/assets/images/icon-sunny.webp",
+      temp: "22°",
+    },
+    {
+      time_stamp: "3 PM",
+      icon: "public/assets/images/icon-sunny.webp",
+      temp: "22°",
+    },
+  ];
   const InputPlaceholder = () => {
     return (
       <>
@@ -42,7 +127,7 @@ const App = () => {
       <div className="title-container">
         <p className="title">How's the sky looking today?</p>
       </div>
-      <main className="content-container">
+      <main className="main-content-container">
         <div className="search-container">
           <input
             id="search-input"
@@ -55,6 +140,7 @@ const App = () => {
         </div>
         <div className="content-container">
           <div className="left-content">
+            {/* Weather Information Section */}
             <div className="weather-info">
               <img
                 src="assets/images/bg-today-large.svg"
@@ -65,22 +151,54 @@ const App = () => {
                 <p>Tuesday, Aug 5, 2025</p>
               </div>
               <div className="temperature-info">
-                <img src="assets/images/icon-sunny.webp" alt="sunny-icon" />
+                <img
+                  src="public/assets/images/icon-sunny.webp"
+                  alt="sunny-icon"
+                />
                 <p>20</p>
               </div>
             </div>
+            {/* Temperature Details Section */}
+            <div className="temperature-details">
+              {temperature_details.map((detail) => (
+                <div className="detail-item card-item" key={detail.label}>
+                  <p>{detail.label}</p>
+                  <p>{detail.value}</p>
+                </div>
+              ))}
+            </div>
+            {/* Daily Forcast Section */}
+            <div className="daily-forcast">
+              <p>Daily Forcast</p>
+              <div className="forcast-details">
+                {daily_forcast.map((day) => (
+                  <div className="forcast-item card-item" key={day.day}>
+                    <p>{day.day}</p>
+                    <img src={day.icon} alt={`${day.day}-icon`} />
+                    <div className="temp-range">
+                      <p>{day.max_temp}</p>
+                      <p>{day.min_temp}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="characteristics">
-            <p>Feels Like</p>
-            <p>Humidity</p>
-            <p>Wind</p>
-            <p>Precipitation</p>
+          <div className="hourly-forecast">
+            <div className="hourly-forecast-header">
+              <p>Hourly Forecast</p>
+              <Dropdown options={week_options} />
+            </div>
+            {hourly_forcast?.map((item) => (
+              <div className="hourly-forcast-item">
+                <img src={item.icon} />
+                <p>{item.time_stamp}</p>
+                <p>{item.temp}</p>
+              </div>
+            ))}
           </div>
-          <p>Daily Forcast</p>
-          <div>{/* Future daily forecast components will go here */}</div>
         </div>
       </main>
-      <aside></aside>
     </div>
   );
 };
