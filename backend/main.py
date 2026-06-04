bd6d4d02c4e20132f4082e9085637ee7111b391a
Komal Rout <komalrout.kr@gmail.com>
2026-06-04 23:39:47 +0530

    Added deployment config

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://komalrout.github.io"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
