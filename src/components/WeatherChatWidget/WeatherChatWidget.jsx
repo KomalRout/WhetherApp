62ceb9d27281bd36d79d48c3ebfde3edbebbdbc4
Komal Rout <komalrout.kr@gmail.com>
2026-06-05 00:15:15 +0530

    fixed import issue

--- src/components/WeatherChatWidget/WeatherChatWidget.jsx
+++ src/components/WeatherChatWidget/WeatherChatWidget.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import "./WeatherChatWidget.css";
+import "./weatherChatWidget.css";
 import { useWeatherAgent } from "../../hooks/useWeatherAgent";
 
 const SUGGESTIONS = [
